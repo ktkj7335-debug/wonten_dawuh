6347ed87021ec4855aa12006d09185db303a75fe
ktkj7335-debug <ktkj7335@gmail.com>
2025-10-06 23:22:38 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombo berdasarkan ID-nya
+const tombolSapa = documen.getElementById('sapaButton');
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('clik', function() {
+  // 3. Jalankan kode ini ketika tombo di-klik
+  alert('Halo! suwun ws mrene!');
+});
